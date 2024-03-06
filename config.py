d2228ab99882e89640477c8c40f4d74782f6a1d4
tushar007ff <155503833+tushar007ff@users.noreply.github.com>
2024-03-06 11:02:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,15 +39,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/akshayxt/Itachi",
+    "https://github.com/tushar007ff/test",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TeamXT_Support")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TeamXT_Support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/warzone_123")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/warzone_123")
 
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
 
